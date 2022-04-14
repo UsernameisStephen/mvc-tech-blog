f10351ae161db30b79842cb70a878339058bea95
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-04-13 22:29:25 -0500

    added put and delete route to post route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,13 @@
+// An index file to gather the API routes and export them for use
+// Dependencies
+// Server connection
+
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
 
+// Define route path for the API to use, e.g. api/users/
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const { Post } = require('../../models/');
 const withAuth = require('../../utils/auth');
-const sequelize = require('../../config/connection');
 // GET api/posts/ -- get all posts
 router.post('/', withAuth, async (req, res) => {
     const body = req.body;
@@ -49,40 +48,43 @@ router.post('/', async (req, res) => {
 
 // PUT api/posts/1-- update a post's title or text
 
-router.put('/:id', async (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', withAuth, async (req, res) => {
   try {
-    const updateTag = await Tag.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-    tag_name: req.body.tag_name
-  })
-  res.status(200).json(updateTag);
+    const [affectedRows] = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
 // DELETE api/posts/1 -- delete a post
 
-router.delete('/:id', async (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const deleteTag = await Tag.destroy({
+    const [affectedRows] = Post.destroy({
       where: {
         id: req.params.id,
       },
     });
-   
-    if (!deleteTag) {
-      res.status(404).json({message: 'This is not the Tag you were looking for.'});
-      return;
+
+    if (affectedRows > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
     }
-    res.status(200).json(deleteTag);
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
 
 module.exports = router;
