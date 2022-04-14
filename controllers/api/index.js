ebc3e13dbe0724dcdb95013ff9e5067cff61d0d5
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-04-13 22:49:54 -0500

    added route path for comment route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,9 +5,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 
 // Define route path for the API to use, e.g. api/users/
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comment', commentRoutes);
+
 
 module.exports = router;
